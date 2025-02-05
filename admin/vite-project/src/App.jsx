f86f7126725cfbaf1a63f01af9e14d758265e48c
Ukobo Utibeabasi <96931322+Ticrae@users.noreply.github.com>
2025-02-05 18:19:49 -0500

    Update App.jsx

--- admin/vite-project/src/App.jsx
+++ admin/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import { Route, Routes } from "react-router-dom";
