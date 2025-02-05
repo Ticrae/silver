bb49b8d1d94ab0e81f116fc88ada37cd67e90a9d
Ukobo Utibeabasi <ukoboutibe@yahoo.com>
2025-02-04 19:52:20 -0500

    so

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,11 @@ import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json());
-app.use(cors({
-  origin: [""],
-  methods: ["POST","GET"],
-  credentials: true
-}));
+app.use(cors());
 
 // db connection
 connectDB();
