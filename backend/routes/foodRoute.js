4f71850f33f5d2a17255b8929c69e9b8b2188e14
Ukobo Utibeabasi <96931322+Ticrae@users.noreply.github.com>
2025-02-05 16:42:43 -0500

    Update foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -11,9 +11,9 @@ const foodRouter = express.Router();
 // Image Storage Engine
 const storage = multer.diskStorage({
   destination: "uploads",
-  filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}${file.originalname}`);
-  },
+  // filename: (req, file, cb) => {
+  //   return cb(null, `${Date.now()}${file.originalname}`);
+  // },
 });
 
 const upload = multer({ storage: storage });
