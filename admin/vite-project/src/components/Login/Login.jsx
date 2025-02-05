8ab87a8c5d2bd173dae01fd0326ed2fb7c721191
Ukobo Utibeabasi <96931322+Ticrae@users.noreply.github.com>
2025-02-04 22:20:02 -0500

    Update Login.jsx

--- admin/vite-project/src/components/Login/Login.jsx
+++ admin/vite-project/src/components/Login/Login.jsx
@@ -19,7 +19,7 @@ const Login = ({ setToken, url }) => {
   const onLogin = async (event) => {
     try {
       event.preventDefault();
-      const response = await axios.post(`${url}api/user/admin, data);
+      const response = await axios.post(`${url}api/user/admin`, data);
 
       if (response.data.success) {
         setToken(response.data.token);
