c51a651bc1ab8973a4efb1b3e9c923cbdc3ea027
Ukobo Utibeabasi <96931322+Ticrae@users.noreply.github.com>
2025-02-04 22:37:22 -0500

    Update Orders.jsx

--- admin/vite-project/src/pages/Orders/Orders.jsx
+++ admin/vite-project/src/pages/Orders/Orders.jsx
@@ -20,7 +20,7 @@ const Orders = ({ url }) => {
   };
 
   const statusHandler = async (event, orderId) => {
-    const response = await axios.post(url + "/api/order/status", {
+    const response = await axios.post(url + "api/order/status", {
       orderId,
       status: event.target.value,
     });
